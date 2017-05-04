be37d56cec20c00f2b0e66da96f7da0af84d42d0
Fabio NET Hackattak <randomlife@msn.com>
2017-05-04 22:49:16 +0200

    aggiunta descrizione al programma

--- Win_Fake_SMailer/Win_Fake_SMailer/Win_Fake_SMailer/Form1.Designer.cs
+++ Win_Fake_SMailer/Win_Fake_SMailer/Win_Fake_SMailer/Form1.Designer.cs
@@ -456,7 +456,7 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(192, 74);
+            this.tabPage2.Size = new System.Drawing.Size(192, 222);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "tabPage2";
             this.tabPage2.UseVisualStyleBackColor = true;
